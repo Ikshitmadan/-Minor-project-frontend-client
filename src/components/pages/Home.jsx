5e70756af5eeb24fb3c4459d907e020f6b74718f
ikshitmadan <ikshitmadan71@gmail.com>
2023-09-23 22:23:27 +0530

    private-route and chatBox

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../Layout/Layout'
+import { useRef } from 'react'
+import io from 'socket.io-client'
+import { ChatBot } from '../ChatBot'
 
 const Home = () => {
+
+    const [socket,setsocket]=useState();
+
+    useEffect(()=>{
+       
+                const socket = io('http://localhost:8080');
+        console.log(socket);
+       setsocket(socket)
+              
+                return ()=>{
+        setsocket(null);
+        
+                }
+            },[])
+
     return (
-        <Layout>
-            <h1>Home</h1>
-        </Layout>
+        <div>
+<ChatBot socket={socket}/>
+
+            
+</div>
     )
 }
 
